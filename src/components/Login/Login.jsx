a190a119e7e24f1762af6431f6ee3f9cf55f29a6
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-10-31 19:45:00 +0600

    login and register creadted

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,54 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
+
+const handleLogin = e =>{
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const name = e.target.name.value;
+    console.log(name ,email,password);
+}
+
+
     return (
-        <div>
-            this is login
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">Enjoy the world of<br /><span className="uppercase font-thin">Auth-er-moha-milon</span></p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" name="name" placeholder="Your Name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            <p className="p-2 text-center m-2 font-medium  italic">New to Auth?<Link to="/register"><button className="btn btn-link -ml-2">Register here</button></Link></p>
+          </div>
         </div>
+      </div>
     );
 };
 
